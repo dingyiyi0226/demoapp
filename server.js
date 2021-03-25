215f7d743bf9ed55cf1f0746c4d87c018940205d
Huan-Ting,Chen <dingyiyi0226@gmail.com>
2021-03-25 09:57:43 +0800

    simple web app

---
+++ server.js
@@ -0,0 +1,16 @@
+import cors from 'cors'
+import express from 'express'
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Debug function
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!')
+});
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`)
+})
